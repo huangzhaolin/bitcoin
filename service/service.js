b5da128e9a7e0dd56afdd15168a106e6bd133602
朝霖 <zhaolin.huang@alipay.com>
2013-07-22 14:09:33 +0800

    update

--- service/service.js
+++ service/service.js
@@ -6,7 +6,7 @@ var logger = require('../logger.js');
 var utils = require('util');
 var fs = require('fs');
 //获取交易数据
-export.getTradeData=function(parameters,res){
+exports.getTradeData=function(parameters,res){
 	var querySql=utils.format("select * from trade_data where date_time >= '%s' and date_time <='%s' and orignal = '%s'",parameters.startTime,parameters.endTime,parameters.orignal)
 	dao.execute(querySql,function(data){
 		res.send(data);
