2eebefc28d6acdb915cdeff563345817520c1469
jolinhuang <xsjcy2bhzl@gmail.com>
2013-07-29 23:39:06 +0800

    m
    m

--- public/javascripts/bitcoin.js
+++ public/javascripts/bitcoin.js
@@ -8,7 +8,7 @@ bitcoin.requestData=function(){
 	$.ajax({
 		url:"queryData.htm",
 		type:"post",
-		data:{startTime:"2013-07-01 00:00:00",endTime:"2013-07-30 00:00:00",orignal:"BTCCHINA"}
+		data:{startTime:"2013-07-01 00:00:00",endTime:"2013-07-30 00:00:00",orignal:"BTCCHINA"},
 		success:function(jsonData){
 			console.log(jsonData);
 		}
